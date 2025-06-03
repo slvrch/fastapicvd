c9a171338563d1505a312b01197d2e55d60f45bc
slvrch <slvrachma@gmail.com>
2025-06-04 01:03:22 +0700

    Update supabase_client

--- supabase_client.py
+++ supabase_client.py
@@ -5,13 +5,12 @@ import os
 # Load environment variables from .end file
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL", "")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+# Validasi env vars
 if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("SUPABASE_URL and SUPABASE_KEY belum diset di .env atau environment variabel.")
-print("SUPABASE_URL:", SUPABASE_URL)
-print("SUPABASE_KEY:", SUPABASE_KEY)
+    raise ValueError("SUPABASE_URL and SUPABASE_KEY belum diset di environment variables.")
 
 # Membuat klien Supabase
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
